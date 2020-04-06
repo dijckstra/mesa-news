35e565c8c20b804de0b686e4a0b501e399802fc7
dijckstra <dickssonoliveira@gmail.com>
2020-04-06 12:43:03 -0300

    feat(data): add db logic to local data source

--- app/src/main/java/inc/mesa/mesanews/data/source/local/NewsLocalDataSource.java
+++ app/src/main/java/inc/mesa/mesanews/data/source/local/NewsLocalDataSource.java
@@ -1,37 +1,68 @@
 package inc.mesa.mesanews.data.source.local;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import inc.mesa.mesanews.data.News;
 import inc.mesa.mesanews.data.source.NewsDataSource;
+import inc.mesa.mesanews.executors.AppExecutors;
 
 public class NewsLocalDataSource implements NewsDataSource {
 
-    private static List<News> mockNews = Collections.singletonList(
-            new News(1,
-                     "Amazon, Walmart and others battle price gouging on coronavirus-related products - CNBC",
-                     "Amazon, eBay, Walmart and Etsy have all struggled to curb price gouging and products that make unverified claims about the coronavirus.",
-                     "https://www.cnbc.com/2020/03/03/amazon-walmart-e-retailers-battle-price-gouging-on-coronavirus-products.html",
-                     "https://image.cnbcfm.com/api/v1/image/106422372-1583253995153rtx7ashh.jpg?v=1583254080",
-                     false)
-    );
+    private AppExecutors executors;
+    private NewsDao newsDao;
 
-    @Override
-    public void getNews(final boolean highlights, final NewsResult callback) {
-        callback.onNewsLoaded(new ArrayList<>());
+    public NewsLocalDataSource(final AppExecutors executors, final NewsDao newsDao) {
+        this.executors = executors;
+        this.newsDao = newsDao;
     }
 
     @Override
-    public void getNewsArticle(final int newsId, final ArticleResult callback) {
-        callback.onArticleLoaded(mockNews.get(newsId));
+    public void getNews(final int currentPage, final int perPage, final NewsResult callback) {
+        executors.diskIO().execute(() -> {
+            int offset = (currentPage - 1) * perPage;
+
+            List<News> news = newsDao.getNews(perPage, offset);
+            handleResult(news, callback);
+        });
     }
 
     @Override
-    public void toggleFavorite(final int newsId) {
-        News news = mockNews.get(newsId);
-        boolean b = news.isFavorite();
-        news.setFavorite(!b);
+    public void getHighlights(final NewsResult callback) {
+        executors.diskIO().execute(() -> {
+            List<News> news = newsDao.getHighlights();
+            handleResult(news, callback);
+        });
+    }
+
+    private void handleResult(final List<News> news, final NewsResult callback) {
+        executors.mainThread().execute(() -> {
+            if (news.isEmpty()) {
+                callback.onDataNotAvailable();
+            } else {
+                callback.onNewsLoaded(news);
+            }
+        });
+    }
+
+    @Override
+    public void getNewsArticle(final String newsId, final ArticleResult callback) {
+        executors.diskIO().execute(() -> {
+            News news = newsDao.getNewsById(newsId);
+            executors.mainThread().execute(() -> {
+                if (news != null) {
+                    callback.onArticleLoaded(news);
+                }
+            });
+        });
+    }
+
+    @Override
+    public void addNewsArticle(final News news) {
+        executors.diskIO().execute(() -> newsDao.insertNewsArticle(news));
+    }
+
+    @Override
+    public void toggleFavorite(final String newsId) {
+        executors.diskIO().execute(() -> newsDao.toggleFavorite(newsId));
     }
 }
